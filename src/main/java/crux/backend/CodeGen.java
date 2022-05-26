2444c701661f6190f1001f9a4a7b818b225a5c00
wallyy <Theflash@2>
2022-05-25 19:20:05 -0700

    Test driven development is kinda tough on this project, just working out the easy functions that I might also think will help with passing the first test. Finished call and return

--- src/main/java/crux/backend/CodeGen.java
+++ src/main/java/crux/backend/CodeGen.java
@@ -5,6 +5,7 @@ import crux.ir.*;
 import crux.ir.insts.*;
 import crux.printing.IRValueFormatter;
 
+import java.lang.reflect.Parameter;
 import java.util.*;
 
 /**
@@ -13,11 +14,11 @@ import java.util.*;
 public final class CodeGen extends InstVisitor {
   private final Program p;
   private final CodePrinter out;
-  private String[] regs = { "%rdi", "%rdx",
+  private final String[] regs = { "%rdi", "%rdx",
     "%rsi", "%rcx", "%r8", "%r9"};
   private HashMap<Variable, Integer> varIndexMap;
   private int varIndex;
-  private HashSet<Instruction> discovered = new HashSet<>();
+  private final HashSet<Instruction> discovered = new HashSet<>();
   private HashMap<Instruction, String> currLabelMap;
 
   public CodeGen(Program p) {
@@ -41,7 +42,7 @@ public final class CodeGen extends InstVisitor {
               + ((IntegerConstant) (g.getNumElement())).getValue() * 8 + ",8");
 
     }
-    int count [] = new int[1];
+    int[] count = new int[1];
     for(Iterator<Function> func_it = p.getFunctions(); func_it.hasNext();){
       Function f = func_it.next();
       genCode(f, count);
@@ -63,16 +64,20 @@ public final class CodeGen extends InstVisitor {
       if (varIndex <= 6) {
         out.bufferCode("movq " + regs[varIndex - 1] + ", " + (-8 * argIndex) + "(%rbp)");
       }else {
-        out.bufferCode("push" + variable.getName());
+        out.bufferCode("movq " + (16 + 8 * (argIndex-7)) + "(%rbp), %r10");
+        out.bufferCode("movq %r10 " + (-8 * argIndex) + "(%rbp)");
       }
       //transform to CFG
       //use dfs
       if(f.getStart() != null){
         discovered.clear();
-        dfs(f.getStart())
+        dfs(f.getStart());
       }
-
-
+      if (varIndex % 2 == 0){
+        out.printCode("enter $(8 * " + (varIndex) + "), $0");
+      }else{
+        out.printCode("enter $(8 * " + (varIndex + 1 ) + "), $0");
+      }
 
     }
 
@@ -103,15 +108,23 @@ public final class CodeGen extends InstVisitor {
   }
 
 
-
-      }
-
+  public void visit(AddressAt i) {
+    if(i.getOffset() == null) {
+      out.bufferCode("movq " + i.getBase().getName() + "@GOTPCREL(%rip), %r11");
+      out.bufferCode("movq %r11, " + varIndexMap.get(i.getDst()) + "(%rbp)");
     }
+    if(i.getOffset() != null) {
+        out.bufferCode("movq " + i.getBase().getName() + "@GOTPCREL(%rip), %r11");
+        out.bufferCode("movq " + varIndexMap.get(i.getOffset()) + "(%rbp), %r10");
+        out.bufferCode("imulq $8, %r10");
+        out.bufferCode("addq %r10, %r11");
+        out.bufferCode("movq %r11, " + varIndexMap.get(i.getDst()) + "(%rbp)");
+      }
   }
 
-  public void visit(AddressAt i) {}
-
   public void visit(BinaryOperator i) {
+
+
     out.printCode("movq " + (-8 * varIndexMap.get(i.getLeftOperand())) + "(%rdi), %r8");
     switch (i.getOperator()) {
       case Add:
@@ -127,27 +140,59 @@ public final class CodeGen extends InstVisitor {
         out.printCode("divq" + (-8 * varIndexMap.get(i.getRightOperand())) + "%rdi, %r8");
         break;
     }
-//    if (varIndexMap.get(i.getDst()) == null) {
-//      varIndexMap.put(i.getDst(), ++ stackSize);
-//    }
     out.printCode("movq %r10, " + (-8 * varIndexMap.get(i.getDst())) + "%rdi");
   }
 
   public void visit(CompareInst i) {}
 
-  public void visit(CopyInst i) {}
+  public void visit(CopyInst i) {
 
-  public void visit(JumpInst i) {}
+    if(i.getSrcValue() instanceof  IntegerConstant){
+    }
+  }
 
-  public void visit(LoadInst i) {}
+  public void visit(JumpInst i) {
+    out.bufferCode("movq " + (-8 * varIndexMap.get(i.getPredicate())) + "(%rbp), %r10");
+    out.bufferCode("cmp $1, %r10");
+    out.bufferCode("je " + currLabelMap.get(i.getNext(1)));
+  }
 
-  public void visit(NopInst i) {}
+  public void visit(LoadInst i) {
+
+  }
+
+  public void visit(NopInst i) {
+
+  }
 
   public void visit(StoreInst i) {}
 
-  public void visit(ReturnInst i) {}
+  public void visit(ReturnInst i) {
+    out.bufferCode("movq " + varIndexMap.get(i.getReturnValue()) + "(%rbp), %rax");
+    out.bufferCode("leave");
+    out.bufferCode("ret");
+}
 
-  public void visit(CallInst i) {}
 
-  public void visit(UnaryNotInst i) {}
+  public void visit(CallInst i) {
+    int numOfArgs = 0 ;
+    for(LocalVar param : i.getParams()){
+      numOfArgs++;
+      if(numOfArgs > 6) {
+        out.bufferCode("movq " + (varIndexMap.get(param)) + "(%rbp), %r10");
+        out.bufferCode("movq %r10, " + (16 + 8 * (numOfArgs - 7)) + "(%rbp)");
+      }else {
+        out.bufferCode("movq " + (varIndexMap.get(param)) + "(%rbp), " + regs[numOfArgs - 1]);
+      }
+      out.bufferCode("call " + i.getCallee().getName());
+      out.bufferCode("movq %rax, " + (-8 * varIndexMap.get(i.getDst()) ) + "(%rbp)");
+
+      }
+    }
+
+  public void visit(UnaryNotInst i) {
+    out.bufferCode("movq " + varIndexMap.get(i.getInner()) + "(%rbp), %r10");
+    out.bufferCode("subq %r10, " + varIndexMap.get(i.getDst()) + "(%rbp)");
+
+  }
 }
